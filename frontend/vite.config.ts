0d20d9b19498306906707dfe5a33eb5d2ffff045
cetusw <kugelev.m@gmail.com>
2026-07-28 18:23:45 +0300

    DORM-86 Добавил сертификаты и дописал конфиг, чтобы сайт работал через https, поменял иконку, поправил пути

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'node:fs'
+import path from 'node:path'
 
 export default defineConfig({
-  base: '/app/',
-  plugins: [react()],
-  build: {
-    outDir: '../web/app',
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
+    base: '/app/',
+    plugins: [react()],
+    build: {
+        outDir: '../web/app',
+        emptyOutDir: true,
     },
-  },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+
+        https: {
+            key: fs.readFileSync(
+                path.resolve(__dirname, '.cert/dev-key.pem'),
+            ),
+            cert: fs.readFileSync(
+                path.resolve(__dirname, '.cert/dev-cert.pem'),
+            ),
+        },
+
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
